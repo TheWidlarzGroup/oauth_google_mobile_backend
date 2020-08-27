aa89983d7612d703248af513d61f687659262b6d
Daniel Grychtol <grychtol.daniel@gmail.com>
2020-08-27 21:10:28 +0200

    Remove redundant dotenv, add morgan for logging

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
-require("dotenv").config();
-
 import * as Express from "express";
+import * as morgan from "morgan";
 
 const app = Express();
 const port = process.env.PORT || 7000;
+
+if (process.env.MODE === "dev") {
+  app.use(morgan("tiny"));
+}
 
 app.get("/", (_: Express.Request, res: Express.Response) => {
   res.send("Hello world");
