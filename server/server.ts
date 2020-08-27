2e8ef3b12a72f834fac960bdb0f3cf063f392d4a
Daniel Grychtol <grychtol.daniel@gmail.com>
2020-08-27 21:55:09 +0200

    Add static content

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,19 @@
 import * as Express from "express";
 import * as morgan from "morgan";
+import * as path from "path";
 
 const app = Express();
 const port = process.env.PORT || 7004;
+
+// app.use(Express.static("public"));
 
 if (process.env.MODE === "dev") {
   app.use(morgan("tiny"));
 }
 
 app.get("/", (_: Express.Request, res: Express.Response) => {
-  res.send("Hello world");
+  // res.send("Hello world");
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 app.listen(port, () => {
