b7a023a85bc930085e91921bb4d7e74ee786e439
Daniel Grychtol <grychtol.daniel@gmail.com>
2020-09-11 17:03:45 +0200

    Add redirect route

--- server/server.ts
+++ server/server.ts
@@ -18,6 +18,10 @@ if (process.env.MODE === "dev") {
   app.use(morgan("tiny"));
 }
 
+app.get("/student_oauth_redirect", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "test.html"));
+});
+
 app.get("/", (_: Express.Request, res: Express.Response) => {
   // res.send("Hello world");
   res.sendFile(path.join(__dirname, "../public", "index.html"));
