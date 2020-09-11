360a0a0fbd6d678ac3015905fcb6a34ea55ae6b3
Daniel Grychtol <grychtol.daniel@gmail.com>
2020-09-11 18:23:18 +0200

    asd

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,8 @@ app.use(
   "/.well-known",
   Express.static(path.join(__dirname, "../public"), {
     setHeaders: (res) => {
-      res.type("application/json");
+      // res.type("application/json");
+      res.type("application/pkcs7-mime");
     },
   })
 );
