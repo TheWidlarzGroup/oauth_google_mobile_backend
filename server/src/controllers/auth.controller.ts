a27b3530fbf1e51aa5a373b0436000cf72cc221d
Daniel Grychtoł <grychtol.daniel@gmail.com>
2021-09-10 10:58:25 +0200

    Update oauth redirect handler

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import * as Express from "express";
 import { generateJWTToken } from "../../src/utils/jwt";
+import * as path from "path";
 
 export const handleOAuthRedirect = (
   req: Express.Request,
@@ -12,6 +13,6 @@ export const handleOAuthRedirect = (
 };
 
 export const oauthRedirect = (req: Express.Request, res: Express.Response) => {
-  console.log(req.query);
   res.status(200).type("text/html").send("");
+  res.sendFile(path.join(__dirname, "../../public", "oauth_redirect.html"));
 };
