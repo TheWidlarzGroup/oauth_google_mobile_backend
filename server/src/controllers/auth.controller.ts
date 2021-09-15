9c43bf497675c7032ac45d99a200a519c0c9c36c
Daniel Grychtoł <grychtol.daniel@gmail.com>
2021-09-15 17:29:10 +0200

    Add email trigger from within handleOauthRedirect

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,12 +1,18 @@
 import * as Express from "express";
 import { generateJWTToken } from "../../src/utils/jwt";
 import * as path from "path";
+import * as Mailer from "../services/mailer";
 
 export const handleOAuthRedirect = (
   req: Express.Request,
   res: Express.Response
 ) => {
-  const token = generateJWTToken(req?.user?.id ?? "");
+  const { id: userId, email } = req?.user || { id: "", email: "" };
+  const token = generateJWTToken(userId);
+
+  if (email) {
+    Mailer.sendAccountConfirmationEmail(email);
+  }
 
   res.redirect("/auth/oauth_redirect/" + token);
 };
